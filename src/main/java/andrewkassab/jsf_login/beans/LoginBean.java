9bd2b15456cdd66bdc2cb923ee4c28feb07da0a7
andrewkassab <andrewkassab@live.com>
2024-09-18 21:08:09 -0700

    added save method to service, added registration method to login baen

--- src/main/java/andrewkassab/jsf_login/beans/LoginBean.java
+++ src/main/java/andrewkassab/jsf_login/beans/LoginBean.java
@@ -1,17 +1,14 @@
 package andrewkassab.jsf_login.beans;
 
 import andrewkassab.jsf_login.model.User;
-import andrewkassab.jsf_login.service.UserService;
+import andrewkassab.jsf_login.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 
-import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.IOException;
+import java.util.Optional;
 
 @Named("loginBean")
 @SessionScoped
@@ -20,14 +17,35 @@ public class LoginBean {
     private String username;
     private String password;
 
+    private String confirmPassword;
+
     @Autowired
-    private UserService userService;
+    private UserServiceImpl userService;
 
     public void login() throws IOException {
         if (userService.login(username, password).isPresent()) {
             FacesContext.getCurrentInstance().getExternalContext().redirect("welcome.xhtml");
         } else {
             FacesContext.getCurrentInstance().getExternalContext().redirect("error.xhtml");
+        }
+    }
+
+    public void register() throws IOException {
+        if (!password.equals(confirmPassword)) {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("error.xhtml?error=password_mismatch");
+            return;
+        }
+
+        Optional<User> existingUser = userService.findByUsername(username);
+        if (existingUser.isPresent()) {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("error.xhtml?error=user_exists");
+            return;
+        } else {
+            User newUser = new User();
+            newUser.setUsername(username);
+            newUser.setPassword(password);
+            userService.saveUser(newUser);
+            FacesContext.getCurrentInstance().getExternalContext().redirect("registration_success.xhtml");
         }
     }
 
@@ -47,11 +65,19 @@ public class LoginBean {
         this.password = password;
     }
 
-    public UserService getUserService() {
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public UserServiceImpl getUserService() {
         return userService;
     }
 
-    public void setUserService(UserService userService) {
+    public void setUserService(UserServiceImpl userService) {
         this.userService = userService;
     }
 
--- src/main/java/andrewkassab/jsf_login/service/UserService.java
+++ src/main/java/andrewkassab/jsf_login/service/UserService.java
@@ -1,23 +1,15 @@
 package andrewkassab.jsf_login.service;
 
-import andrewkassab.jsf_login.dao.UserDAO;
 import andrewkassab.jsf_login.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    @Autowired
-    private UserDAO userDAO;
+    Optional<User> login(String username, String password);
 
-    @Transactional
-    public Optional<User> login(String username, String password) {
-        Optional<User> user = userDAO.findUserByUsername(username);
-        return user.filter(u -> u.getPassword().equals(password));
-    }
+    Optional<User> findByUsername(String username);
+
+    void saveUser(User user);
 
 }
